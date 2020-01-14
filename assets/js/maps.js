9ed98ae77315456bab867805db8d97dc29ccdbf4
Ronan C <rojocas@gmail.com>
2020-01-14 15:40:50 +0000

    Randomly selecting locations from array to similate user location

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,29 +1,62 @@
+// function initMap() {
+    
+//     var map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 3,
+//         center: {
+//             lat: 46.619261,
+//             lng: -33.134766
+//         }
+//     });
+
+//     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//     var locations = [
+//         { lat: 40.785091, lng: -73.968285 },
+//         { lat: 41.084045, lng: -73.874245 },
+//         { lat: 40.754932, lng: -73.984016 }
+//     ];
+
+//     var markers = locations.map(function (location, i) {
+//         return new google.maps.Marker({
+//             position: location,
+//             label: labels[i % labels.length]
+//         });
+//     });
+
+//     var markerCluster = new MarkerClusterer(map, markers,
+//         { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+// }
+
 function initMap() {
-
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
-        }
-    });
-
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
-    ];
+        { lat: 52.017949, lng:  -9.493964 },
+        { lat: 45.868655, lng: 6.185286 },
+        { lat: -4.439318, lng: 15.258601 },
+        { lat: 42.972104, lng: -71.696054},
+        { lat: 43.146907,lng: 131.996560}
+     ];
 
-    var markers = locations.map(function (location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
+    // Select random location from array
+    var location = locations[Math.floor(Math.random() * locations.length)];
+
+    console.log(location);
+
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: location,
+        zoom: 16
     });
 
-    var markerCluster = new MarkerClusterer(map, markers,
-        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    // var markers = locations.map(function (location, i) {
+    //     return new google.maps.Marker({
+    //         position: location,
+    //         label: labels[i % labels.length]
+    //     });
+    // });
+
+    // var markerCluster = new MarkerClusterer(map, markers,
+    //     { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
 }
